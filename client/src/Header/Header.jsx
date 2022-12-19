2e2c9417027c941a6285442a67c1e6783be23e1f
Bhasim <bahaholding@aol.com>
2022-12-19 17:25:08 +0100

    update

--- client/src/Header/Header.jsx
+++ client/src/Header/Header.jsx
@@ -3,8 +3,8 @@ import {Link} from "react-router-dom"
 import "./header.scss"
 import logo from "../images/logo.png"
 import { MdOutlineMenuBook } from "react-icons/md"
-import {HiOutlineMail} from "react-icons/hi"
-import {FiShoppingCart,FiHome} from "react-icons/fi"
+import {GrMail} from "react-icons/gr"
+import {FaShoppingCart,FaHome} from "react-icons/fa"
 import {SlCalender} from "react-icons/sl"
 import {SiCodechef} from "react-icons/si"
 import {BsQuestionCircle,BsChatDots} from "react-icons/bs"
@@ -35,12 +35,12 @@ function Header() {
 
 
               <Link title='Chat direct with us' to="/userChat"><BsChatDots/></Link>
-              <Link title='Send us your E-Mail' to="/contact"><HiOutlineMail/></Link>
-              <Link title="Go to your Cart" to="/cart"><FiShoppingCart/></Link>
+              <Link title='Send us your E-Mail' to="/contact"><GrMail/></Link>
+              <Link title="Go to your Cart" to="/cart"><FaShoppingCart/></Link>
               <Link title="Book a table" to="/book"><SlCalender/></Link>
               <Link title="Work as a freelancer" to="/freelance"><SiCodechef/></Link>
               <Link title="About us" to="/about"><BsQuestionCircle/></Link>
-                  <Link title="Home" to="/"><FiHome /></Link>
+                  <Link title="Home" to="/"><FaHome /></Link>
                   {isSignedIn ?
                   (<a onClick={handleSignOut} title='Sign out'><FaSignInAlt /></a>)
                   :
